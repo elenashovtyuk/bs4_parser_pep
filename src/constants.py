fdc6c882f3612aeaebb4b24bda2a9856e12f3a73
Elena Shovtyuk <110084590+elenashovtyuk@users.noreply.github.com>
2023-05-30 19:30:34 +0400

    delete comments from constants

--- src/constants.py
+++ src/constants.py
@@ -1,14 +1,9 @@
 from pathlib import Path
 
-# DOWNLOADS_URL = 'https://docs.python.org/3/download.html'
-# WHATS_NEW_URL = 'https://docs.python.org/3/whatsnew/'
+
 MAIN_DOC_URL = 'https://docs.python.org/3/'
-# константа, где будет храниться путь до директории с текущим файлом.
-# Path(__file__) - абсолютный путь до текущего файла;
-# parent - директория, где лежит файл
 BASE_DIR = Path(__file__).parent
 DATETIME_FORMAT = '%Y-%m-%d_%H-%M-%S'
-# добавим новую константу для адреса с PEP документацией
 PEP_URL = 'https://peps.python.org/'
 
 EXPECTED_STATUS = {
